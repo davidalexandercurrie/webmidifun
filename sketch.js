0f24e3a953fce3398b7a1a2130dab284361b03e6
David Currie <david.alexander.currie@gmail.com>
2020-04-18 16:54:18 +1200

    added octave buttons and midi device selection

--- sketch.js
+++ sketch.js
@@ -8,23 +8,70 @@ var currentFill;
 let colours = [];
 myString = "";
 let capture;
+let octave = 0;
+let output = {};
+let midiDeviceConnected = false;
 
-console.log(colours);
 function setup() {
   // put setup code here
-  capture = createCapture(VIDEO);
-  capture.size(320, 240);
+  // capture = createCapture(VIDEO);
+  // capture.size(320, 240);
   createCanvas(300, 600);
   WebMidi.enable(function (err) {
-    output = WebMidi.outputs[0];
+    if (WebMidi.outputs[0]) {
+      output = WebMidi.outputs[0];
+      midiDeviceConnected = true;
+    } else {
+      output = { name: "No midi devices available" };
+    }
     // input.addListener("all", "all", function (e) {
     //   console.log("Received message (" + e.note.name + e.note.octave + ").");
     // });
+    document.getElementById("selected-midi").innerText = output.name;
+    WebMidi.outputs.forEach((e) => {
+      const button = createButton(e.name);
+      button.style("border-radius: 1em");
+      button.parent("select-midi");
+      button.mousePressed(() => {
+        output = output = WebMidi.getOutputByName(e.name);
+        document.getElementById("selected-midi").innerText = output.name;
+        console.log(output.name + " is now selected!");
+      });
+    });
   });
+  document.getElementById("selected-octave").innerText = "\n";
   for (let i = 0; i < 13; i++) {
     colours.push(color(random(255), random(255), random(255)));
   }
-  currentFill = colours[0];
+  currentFill = colours[0 - octave];
+  const octaveUp = createButton("Octave +");
+  octaveUp.style("border-radius: 1em 1em 0 0");
+  octaveUp.mousePressed(() => {
+    if (octave < 12 * 6) {
+      octave = octave + 12;
+    }
+    document.getElementById("selected-octave").innerText =
+      octave > 0
+        ? "+ " + String(octave / 12)
+        : octave < 0
+        ? "- " + Math.abs(String(octave / 12))
+        : "\n";
+  });
+  const octaveDown = createButton("Octave -");
+  octaveDown.style("border-radius: 0em 0em 1em 1em");
+  octaveDown.mousePressed(() => {
+    if (octave > 12 * -2) {
+      octave = octave - 12;
+    }
+    document.getElementById("selected-octave").innerText =
+      octave > 0
+        ? "+ " + String(octave / 12)
+        : octave < 0
+        ? "- " + Math.abs(String(octave / 12))
+        : "\n";
+  });
+  octaveUp.parent("octave");
+  octaveDown.parent("octave");
 }
 
 function draw() {
@@ -44,7 +91,12 @@ function draw() {
   rect(0, 600 - (600 / 12) * (mouseYCapped - 35), 600, 600 / 12);
   rectMode(CENTER);
   fill(currentFill);
-  rect(mouseX, mouseY, 600 / 12, 600 / 12);
+  rect(
+    mouseX,
+    mouseY,
+    ((600 / 12) * mouseX) / 300 + 20,
+    ((600 / 12) * mouseX) / 300 + 20
+  );
   if (playnote) {
     if (mouseY > 600) {
       mouseYCapped = 600;
@@ -65,14 +117,15 @@ function draw() {
 
     mouseYCapped = 600 - mouseYCapped;
     mouseYCapped = Math.floor((mouseYCapped / 600) * 12 + 36);
-    console.log(mouseYCapped - 36);
     fill(colours[mouseYCapped - 36]);
     currentFill = colours[mouseYCapped - 36];
-    output.playNote(mouseYCapped, 1, {
-      duration: 500,
-      velocity: velocity,
-    });
-    playnote = false;
+    if (midiDeviceConnected) {
+      output.playNote(mouseYCapped + octave, 1, {
+        duration: 500,
+        velocity: velocity,
+      });
+      playnote = false;
+    }
   }
   i++;
   if (i % 8 === 0) {
@@ -80,4 +133,35 @@ function draw() {
   }
 }
 
-function mouseClicked() {}
+function keyPressed() {
+  if (key === "ArrowUp") {
+    octaveUp();
+  }
+  if (key === "ArrowDown") {
+    octaveDown();
+  }
+}
+
+function octaveUp() {
+  if (octave < 12 * 6) {
+    octave = octave + 12;
+  }
+  document.getElementById("selected-octave").innerText =
+    octave > 0
+      ? "+ " + String(octave / 12)
+      : octave < 0
+      ? "- " + Math.abs(String(octave / 12))
+      : "\n";
+}
+
+function octaveDown() {
+  if (octave > 12 * -2) {
+    octave = octave - 12;
+  }
+  document.getElementById("selected-octave").innerText =
+    octave > 0
+      ? "+ " + String(octave / 12)
+      : octave < 0
+      ? "- " + Math.abs(String(octave / 12))
+      : "\n";
+}
